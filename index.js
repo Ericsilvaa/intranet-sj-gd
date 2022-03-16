513ea395296cb367a92ef4caa5e3a9fe50f59980
Ericsilvaa <eriicsilva28@gmail.com>
2022-03-16 16:41:58 -0300

    ajustes das paginas fins

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+function initListaGeral() {
+    const tabMenu_lista = document.querySelectorAll('.lista-fins-fundo')
+
+    console.log(tabMenu_lista)
+        if(tabMenu_lista.length) {
+        function activeLine () {
+            this.classList.toggle('ativo')
+            this.nextElementSibling.classList.toggle('ativo')
+        }
+        
+        tabMenu_lista.forEach((item) => {
+            item.addEventListener('click', activeLine)
+        })
+    }
+}
+initListaGeral()
+
+
 function initAccordion() {
     const tabMenu = document.querySelectorAll('.tab-menu-principal')
     
